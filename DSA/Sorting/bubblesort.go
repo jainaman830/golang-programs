7c7f4a3b69718485fcad3f84fa4ee8f193133bb9
Aman <jain.aman830@gmail.com>
2024-02-23 21:41:15 +0530

    bubble sort

---
+++ DSA/Sorting/bubblesort.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func bubbleSort(arr []int) {
+	for i := 0; i < len(arr)-1; i++ {
+		for j := 0; j < len(arr)-i-1; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
+}
+func main() {
+	arr := []int{64, 34, 25, 12, 22, 11, 90}
+	bubbleSort(arr)
+	fmt.Println("Sorted:", arr)
+}
