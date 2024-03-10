49bb1523f17c4a2391b33d2cd6622342ad9ed8fd
Aman <jain.aman830@gmail.com>
2024-03-10 19:41:51 +0530

    check palindrome

---
+++ DSA/Recursion/palindrome.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func CheckPalindrome(s string) bool {
+	s = strings.ToLower(s)
+	s = strings.ReplaceAll(s, " ", "")
+	if s == "" {
+		return false
+	} else if len(s) == 1 {
+		return true
+	} else if s[0] != s[len(s)-1] {
+		return false
+	}
+	i := 0
+	j := len(s) - 1
+	for i < j {
+		if s[i] != s[j] {
+			return false
+		}
+		i++
+		j--
+	}
+	return true
+}
+func main() {
+	fmt.Println(CheckPalindrome("NAN1N"))
+}
