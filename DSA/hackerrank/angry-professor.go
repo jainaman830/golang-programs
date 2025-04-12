a183c311cecd069120a8c123e060b7c4f405427b
Aman <jain.aman830@gmail.com>
2025-04-12 22:09:47 +0530

    angry professor

---
+++ DSA/hackerrank/angry-professor.go
@@ -0,0 +1,97 @@
+/*
+A Discrete Mathematics professor has a class of students. Frustrated with their lack of discipline, the professor decides to cancel class if fewer than some number of students are present when class starts. Arrival times go from on time (arrivalTime<=0) to arrived late (arrivalTime>0).
+
+Given the arrival time of each student and a threshhold number of attendees, determine if the class is cancelled.
+*/
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'angryProfessor' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. INTEGER k
+ *  2. INTEGER_ARRAY a
+ */
+
+func angryProfessor(k int32, a []int32) string {
+	// Write your code here
+	count := int32(0)
+	for _, num := range a {
+		if num <= 0 {
+			count++
+		}
+	}
+	if count < k {
+		return "YES"
+	}
+	return "NO"
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	tTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	t := int32(tTemp)
+
+	for tItr := 0; tItr < int(t); tItr++ {
+		firstMultipleInput := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+		nTemp, err := strconv.ParseInt(firstMultipleInput[0], 10, 64)
+		checkError(err)
+		n := int32(nTemp)
+
+		kTemp, err := strconv.ParseInt(firstMultipleInput[1], 10, 64)
+		checkError(err)
+		k := int32(kTemp)
+
+		aTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+		var a []int32
+
+		for i := 0; i < int(n); i++ {
+			aItemTemp, err := strconv.ParseInt(aTemp[i], 10, 64)
+			checkError(err)
+			aItem := int32(aItemTemp)
+			a = append(a, aItem)
+		}
+
+		result := angryProfessor(k, a)
+
+		fmt.Fprintf(writer, "%s\n", result)
+	}
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
