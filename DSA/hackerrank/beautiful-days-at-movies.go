8cc132b092ebf4bc85eb5a55ce95c0c06c4a0650
Aman <jain.aman830@gmail.com>
2025-04-12 22:43:09 +0530

    beautiful days at movies

---
+++ DSA/hackerrank/beautiful-days-at-movies.go
@@ -0,0 +1,111 @@
+/*
+Lily likes to play games with integers. She has created a new game where she determines the difference between a number and its reverse. For instance, given the number 12, its reverse is 21. Their difference is 9. The number 120 reversed is 21, and their difference is 99.
+
+She decides to apply her game to decision making. She will look at a numbered range of days and will only go to a movie on a beautiful day.
+
+Given a range of numbered days, [i.....j] and a number k, determine the number of days in the range that are beautiful. Beautiful numbers are defined as numbers where |i-reverse(i)| is evenly divisible by k. If a day's value is a beautiful number, it is a beautiful day. Return the number of beautiful days in the range.
+
+# Function Description
+
+Complete the beautifulDays function in the editor below.
+
+beautifulDays has the following parameter(s):
+
+int i: the starting day number
+int j: the ending day number
+int k: the divisor
+Returns
+
+int: the number of beautiful days in the range
+Input Format
+
+A single line of three space-separated integers describing the respective values of i, j, and k.
+*/
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'beautifulDays' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER i
+ *  2. INTEGER j
+ *  3. INTEGER k
+ */
+
+func beautifulDays(i int32, j int32, k int32) int32 {
+	// Write your code here
+	count := 0
+	for a := i; a <= j; a++ {
+		rev := reverse(a, 0)
+		diff := math.Abs(float64(rev - a))
+		if int32(diff)%k == 0 {
+			count++
+		}
+	}
+	return int32(count)
+}
+func reverse(num int32, rev int32) int32 {
+	a := num % 10
+	rev = (rev * 10) + a
+	num = num / 10
+	if num == 0 {
+		return rev
+	}
+	return reverse(num, rev)
+}
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	firstMultipleInput := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	iTemp, err := strconv.ParseInt(firstMultipleInput[0], 10, 64)
+	checkError(err)
+	i := int32(iTemp)
+
+	jTemp, err := strconv.ParseInt(firstMultipleInput[1], 10, 64)
+	checkError(err)
+	j := int32(jTemp)
+
+	kTemp, err := strconv.ParseInt(firstMultipleInput[2], 10, 64)
+	checkError(err)
+	k := int32(kTemp)
+
+	result := beautifulDays(i, j, k)
+
+	fmt.Fprintf(writer, "%d\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
