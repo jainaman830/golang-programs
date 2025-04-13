736ca67ba0da2ce3318daa21ef82632da6de2ad9
Aman <jain.aman830@gmail.com>
2025-04-13 17:53:42 +0530

    save the prisoner

---
+++ DSA/hackerrank/save-the-prisoner.go
@@ -0,0 +1,87 @@
+/*A jail has a number of prisoners and a number of treats to pass out to them. Their jailer decides the fairest way to divide the treats is to seat the prisoners around a circular table in sequentially numbered chairs. A chair number will be drawn from a hat. Beginning with the prisoner in that chair, one candy will be handed to each prisoner sequentially around the table until all have been distributed.
+
+The jailer is playing a little joke, though. The last piece of candy looks like all the others, but it tastes awful. Determine the chair number occupied by the prisoner who will receive that candy.*/
+package main
+
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+)
+
+/*
+ * Complete the 'saveThePrisoner' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER m
+ *  3. INTEGER s
+ */
+
+func saveThePrisoner(n int32, m int32, s int32) int32 {
+    // Write your code here
+    reim := m % n
+    if (reim + s - 1) % n ==0{
+        return n
+    }
+	return (reim + s - 1) % n
+}
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 16 * 1024 * 1024)
+
+    stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+    checkError(err)
+
+    defer stdout.Close()
+
+    writer := bufio.NewWriterSize(stdout, 16 * 1024 * 1024)
+
+    tTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+    checkError(err)
+    t := int32(tTemp)
+
+    for tItr := 0; tItr < int(t); tItr++ {
+        firstMultipleInput := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+        nTemp, err := strconv.ParseInt(firstMultipleInput[0], 10, 64)
+        checkError(err)
+        n := int32(nTemp)
+
+        mTemp, err := strconv.ParseInt(firstMultipleInput[1], 10, 64)
+        checkError(err)
+        m := int32(mTemp)
+
+        sTemp, err := strconv.ParseInt(firstMultipleInput[2], 10, 64)
+        checkError(err)
+        s := int32(sTemp)
+
+        result := saveThePrisoner(n, m, s)
+
+        fmt.Fprintf(writer, "%d\n", result)
+    }
+
+    writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
+
