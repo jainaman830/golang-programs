9328f0a920a1d604b2040ff3da43724f2ae0106d
Aman <jain.aman830@gmail.com>
2025-04-16 19:07:31 +0530

    cut the sticks

---
+++ DSA/hackerrank/cut-the-sticks.go
@@ -0,0 +1,108 @@
+/*
+You are given a number of sticks of varying lengths. You will iteratively cut the sticks into smaller sticks, discarding the shortest pieces until there are none left. At each iteration you will determine the length of the shortest stick remaining, cut that length from each of the longer sticks and then discard all the pieces of that shortest length. When all the remaining sticks are the same length, they cannot be shortened so discard them.
+
+Given the lengths of n sticks, print the number of sticks that are left before each iteration until there are none left.
+*/
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'cutTheSticks' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+func cutTheSticks(arr []int32) []int32 {
+	// Write your code here
+	sort.Slice(arr, func(i, j int) bool { return arr[i] < arr[j] })
+	var output []int32
+	min := arr[0]
+	output = append(output, int32(len(arr)))
+	newArr := arr
+	for {
+		var temp []int32
+		for i := 0; i < len(newArr); i++ {
+			num := newArr[i] - min
+			if num != 0 {
+				temp = append(temp, num)
+			}
+		}
+		newArr = temp
+		if len(temp) > 0 {
+			output = append(output, int32(len(temp)))
+		}
+
+		if len(temp) <= 1 {
+			break
+		}
+		min = temp[0]
+
+	}
+
+	return output
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	arrTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var arr []int32
+
+	for i := 0; i < int(n); i++ {
+		arrItemTemp, err := strconv.ParseInt(arrTemp[i], 10, 64)
+		checkError(err)
+		arrItem := int32(arrItemTemp)
+		arr = append(arr, arrItem)
+	}
+
+	result := cutTheSticks(arr)
+
+	for i, resultItem := range result {
+		fmt.Fprintf(writer, "%d", resultItem)
+
+		if i != len(result)-1 {
+			fmt.Fprintf(writer, "\n")
+		}
+	}
+
+	fmt.Fprintf(writer, "\n")
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
