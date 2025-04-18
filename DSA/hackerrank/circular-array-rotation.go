32620b3a820395fe396321e26887b0ef48d5f030
Aman <jain.aman830@gmail.com>
2025-04-18 18:01:17 +0530

    circular array rotation

---
+++ DSA/hackerrank/circular-array-rotation.go
@@ -0,0 +1,124 @@
+/*
+John Watson knows of an operation called a right circular rotation on an array of integers. One rotation operation moves the last array element to the first position and shifts all remaining elements right one. To test Sherlock's abilities, Watson provides Sherlock with an array of integers. Sherlock is to perform the rotation operation a number of times then determine the value of the element at a given position.
+
+For each array, perform a number of right circular rotations and return the values of the elements at the given indices.
+*/
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'circularArrayRotation' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY a
+ *  2. INTEGER k
+ *  3. INTEGER_ARRAY queries
+ */
+
+func circularArrayRotation(a []int32, k int32, queries []int32) []int32 {
+	// Write your code here
+	if len(a) > 1 {
+		a = rotateArray(a, k)
+	}
+
+	var arr []int32
+	for _, val := range queries {
+
+		arr = append(arr, a[val])
+	}
+	return arr
+
+}
+func rotateArray(a []int32, count int32) []int32 {
+	count = count % int32(len(a))
+	for c := 0; c < int(count); c++ {
+		last := a[len(a)-1]
+		for i := len(a) - 1; i > 0; i-- {
+			a[i] = a[i-1]
+		}
+		a[0] = last
+	}
+	return a
+}
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	firstMultipleInput := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	nTemp, err := strconv.ParseInt(firstMultipleInput[0], 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	kTemp, err := strconv.ParseInt(firstMultipleInput[1], 10, 64)
+	checkError(err)
+	k := int32(kTemp)
+
+	qTemp, err := strconv.ParseInt(firstMultipleInput[2], 10, 64)
+	checkError(err)
+	q := int32(qTemp)
+
+	aTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var a []int32
+
+	for i := 0; i < int(n); i++ {
+		aItemTemp, err := strconv.ParseInt(aTemp[i], 10, 64)
+		checkError(err)
+		aItem := int32(aItemTemp)
+		a = append(a, aItem)
+	}
+
+	var queries []int32
+
+	for i := 0; i < int(q); i++ {
+		queriesItemTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+		checkError(err)
+		queriesItem := int32(queriesItemTemp)
+		queries = append(queries, queriesItem)
+	}
+
+	result := circularArrayRotation(a, k, queries)
+
+	for i, resultItem := range result {
+		fmt.Fprintf(writer, "%d", resultItem)
+
+		if i != len(result)-1 {
+			fmt.Fprintf(writer, "\n")
+		}
+	}
+
+	fmt.Fprintf(writer, "\n")
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
