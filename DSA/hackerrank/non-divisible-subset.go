1b770d014da1aae1979d6f522dc33de821fefc0a
Aman <jain.aman830@gmail.com>
2025-04-22 22:06:18 +0530

    non divisble subset

---
+++ DSA/hackerrank/non-divisible-subset.go
@@ -0,0 +1,101 @@
+/*Given a set of distinct integers, print the size of a maximal subset of s where the sum of any 2 numbers in s' is not evenly divisible by k.*/
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'nonDivisibleSubset' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER k
+ *  2. INTEGER_ARRAY s
+ */
+
+func nonDivisibleSubset(k int32, s []int32) int32 {
+	count := make(map[int32]int)
+	for _, num := range s {
+		count[num%k]++
+	}
+	max := int32(0)
+	if count[0] > 0 {
+		max++
+	}
+	for i := int32(1); i <= k; i++ {
+		if count[i] == 0 {
+			continue
+		}
+		if i+i == k {
+			max++
+		} else {
+			if count[i] > count[k-i] {
+				max += int32(count[i])
+			} else {
+				max += int32(count[k-i])
+			}
+			count[i] = 0
+			count[k-i] = 0
+		}
+	}
+	return max
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	firstMultipleInput := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	nTemp, err := strconv.ParseInt(firstMultipleInput[0], 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	kTemp, err := strconv.ParseInt(firstMultipleInput[1], 10, 64)
+	checkError(err)
+	k := int32(kTemp)
+
+	sTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var s []int32
+
+	for i := 0; i < int(n); i++ {
+		sItemTemp, err := strconv.ParseInt(sTemp[i], 10, 64)
+		checkError(err)
+		sItem := int32(sItemTemp)
+		s = append(s, sItem)
+	}
+
+	result := nonDivisibleSubset(k, s)
+
+	fmt.Fprintf(writer, "%d\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
