d175dfb3a301f01f81e86f07d5c6ddd4e15acf5f
Aman <jain.aman830@gmail.com>
2025-04-22 20:34:07 +0530

    sherlock and squares

---
+++ DSA/hackerrank/Sherlock-squares.go
@@ -0,0 +1,87 @@
+/*Watson likes to challenge Sherlock's math ability. He will provide a starting and ending value that describe a range of integers, inclusive of the endpoints. Sherlock must determine the number of square integers within that range.*/
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'squares' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER a
+ *  2. INTEGER b
+ */
+
+func squares(a int32, b int32) int32 {
+	// Write your code here
+	start := math.Sqrt(float64(a))
+	intStart := int64(start)
+	count := int32(0)
+	for {
+		val := int32(intStart * intStart)
+		if val >= a && val <= b {
+			count++
+		} else if val > b {
+			break
+		}
+		intStart++
+	}
+	return count
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	qTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	q := int32(qTemp)
+
+	for qItr := 0; qItr < int(q); qItr++ {
+		firstMultipleInput := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+		aTemp, err := strconv.ParseInt(firstMultipleInput[0], 10, 64)
+		checkError(err)
+		a := int32(aTemp)
+
+		bTemp, err := strconv.ParseInt(firstMultipleInput[1], 10, 64)
+		checkError(err)
+		b := int32(bTemp)
+
+		result := squares(a, b)
+
+		fmt.Fprintf(writer, "%d\n", result)
+	}
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
