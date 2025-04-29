2e76b1640dcf44e3fa2e49ae49c5e4cc7a32e88d
Aman <jain.aman830@gmail.com>
2025-04-29 22:08:32 +0530

    jumping on the clouds

---
+++ DSA/hackerrank/jumping-on-the-clouds.go
@@ -0,0 +1,95 @@
+/*
+There is a new mobile game that starts with consecutively numbered clouds. Some of the clouds are thunderheads and others are cumulus. The player can jump on any cumulus cloud having a number that is equal to the number of the current cloud plus 1 or 2. The player must avoid the thunderheads. Determine the minimum number of jumps it will take to jump from the starting postion to the last cloud. It is always possible to win the game.
+
+For each game, you will get an array of clouds numbered 0 if they are safe or 1 if they must be avoided.
+*/
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'jumpingOnClouds' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY c as parameter.
+ */
+
+func jumpingOnClouds(c []int32) int32 {
+	// Write your code here
+	var steps int32
+	i := 0
+	for {
+		if i+2 < len(c) {
+			if c[i+2] == 0 {
+				i = i + 2
+				steps++
+			} else if c[i+1] == 0 {
+				i = i + 1
+				steps++
+			}
+		} else if i+1 < len(c) {
+			if c[i+1] == 0 {
+				i = i + 1
+				steps++
+			}
+		}
+		if i == len(c)-1 {
+			break
+		}
+	}
+	return steps
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	cTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var c []int32
+
+	for i := 0; i < int(n); i++ {
+		cItemTemp, err := strconv.ParseInt(cTemp[i], 10, 64)
+		checkError(err)
+		cItem := int32(cItemTemp)
+		c = append(c, cItem)
+	}
+
+	result := jumpingOnClouds(c)
+
+	fmt.Fprintf(writer, "%d\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
